0aed815c3649a89d63f6d1fc133e8dbd3b3ded05
Ayodele Olalekan <longyarnz@gmail.com>
2022-02-03 04:10:09 +0100

    feat(main) - add minimal test functions

---
+++ tests/contract.test.js
@@ -0,0 +1,16 @@
+// TODO: ADD TESTS FOR CONTRACT EVENT LISTENER
+
+import request from 'supertest';
+import { App } from '../app.js';
+import { mongoose } from '../connection/database.js';
+
+describe('Test the app root path', () => {
+  test('It should return a 200 status to the GET method', async () => {
+    const response = await request(App).get('/logs');
+    expect(response.statusCode).toBe(200);
+  });
+
+  afterAll((done) => {
+    mongoose.disconnect(done);
+  });
+});
